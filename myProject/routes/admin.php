f2fa70d45857faf6a73772f89d6ee65adfc3c5ae
asmitathapa19 <116329561+asmitathapa19@users.noreply.github.com>
2023-01-09 12:45:39 +0545

    Auth with Middleware

---
+++ myProject/routes/admin.php
@@ -0,0 +1,8 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+Route::middleware('auth', 'is_admin')->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+?>
