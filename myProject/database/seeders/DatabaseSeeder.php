781d6c0a79cd56e004922b38f5ccacefad8331d6
asmitathapa19 <116329561+asmitathapa19@users.noreply.github.com>
2023-01-10 15:22:00 +0545

    Db Seeder for generating dummy data

--- myProject/database/seeders/DatabaseSeeder.php
+++ myProject/database/seeders/DatabaseSeeder.php
@@ -14,15 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-            AdminSeeder::class
-        ]);
+        // $this->call([
+        //     AdminSeeder::class
+        // ]);
         
     }
 }
